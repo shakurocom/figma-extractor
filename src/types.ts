8ebd9ee431c0dee9383bea6c1172a32e4557f724
Andrew Bazhanov <77441033+andrew-bazhanov@users.noreply.github.com>
2024-12-04 10:37:47 +0300

    feat: 🎸 add .cjs config support (#43)
    * fix: 🐛 types
    
    * feat: 🎸 add .cjs config support
    
    * 5.2.0-canary
    
    * docs: ✏️  sync config documentation with actual config types

--- src/types.ts
+++ src/types.ts
@@ -1,48 +1,83 @@
 type SVGCallback = (config?: import('svgo').Config) => import('svgo').Config;
 
 export type IconConfig = {
-  disabled: boolean;
+  disabled?: boolean;
   nodeIds: string[];
-  iconName: (nameFromFigma: string) => string;
+  // custom format icon name
+  iconName?: (nameFromFigma: string) => string;
+  // custom filter icon callback. It allows skipping some unnecessary icons
   skipIcon?: (name: string) => boolean;
   exportPath: string;
+  // If the field is true, then the SVG sprite will be generated
   generateSprite: boolean;
+  // If the field is true, then the file with icons' names as types will be created
   generateTypes: boolean;
+  // If the field is true, then the extractor will only generate SVG sprite from local icons
+  // else all available icons will be downloaded and the sprite will be generated from them
   localIcons?: boolean;
+  // It allows changing svgo config if you need or disable at all
+  // if this field is a function, then the function will be run before svgo optimization. And changed config from this function will be passed to svgo optimization
+  // if this field is boolean and equals `false` then svgo optimization won't be run at all
   optimizeSvg?: false | SVGCallback;
 };
 
 export type Config = {
+  // your Figma api access key
   apiKey: string;
+  // Figma file id
   fileId: string;
+  // path to json variables extracted from figma extension
   jsonVariablesPath: string;
   styles: {
+    // The main path for exporting all requested data
     exportPath: string;
+
+    /*
+      It will parse returned name of colors or etc. and look for allowed themes at the start of names
+      example: for allowedThemes: ['light','dark'] all returned names will be parsed by looking for 'light' or 'dark' at the start of name
+      And if color name is 'light/text/text-900' then it will be transformed to 'text/text-900' and relate to the 'light' theme
+      All others name which are not found by allowed themes will be overlooked.
+     */
     allowedThemes?: string[];
+
+    /**
+      It's used like default variables inside generated CSS variables
+     */
     defaultTheme?: string;
     colors?: {
-      disabled: boolean;
+      disabled?: boolean;
+      // custom key name
       keyName?: (name?: string) => string;
+      // collections names from figma local variables
       collectionNames: string[];
-      groupNames: string[];
+      // group names in collection
+      groupNames?: string[];
     };
     effects?: {
-      disabled: boolean;
-      keyName?: (name?: string, useTheme?: boolean) => string;
+      disabled?: boolean;
+      // custom key name
+      keyName?: (name?: string) => string;
+      // collections names from figma local variables
       collectionNames: string[];
-      groupNames: string[];
+      // group names in collection
+      groupNames?: string[];
     };
     responsive?: {
-      disabled: boolean;
+      disabled?: boolean;
+      // collections names from figma local variables
       collectionNames: string[];
-    };
-    textStyles?: {
-      collectionNames: string[];
-      disabled: boolean;
-      keyName?: (nameFromFigma: string) => string;
-      addStylesWithPrefixScreen?: boolean;
     };
   };
+  textStyles?: {
+    disabled?: boolean;
+    // custom key name
+    keyName?: (nameFromFigma: string) => string;
+    // collections names from figma local variables
+    collectionNames: string[];
+    // This field is to add extra styles with prefix screens
+    addStylesWithPrefixScreen?: boolean;
+  };
+  // Configuration of icons can have more one setting
   icons: IconConfig | IconConfig[];
 };
 
